c9f59f114fecae4b6eeaedfe6e691655462b0ee3
wcmchen <33730138+wcmchen@users.noreply.github.com>
2017-11-16 15:50:15 -0500

    Add files via upload

--- Main.vb
+++ Main.vb
@@ -1,32 +1,12 @@
-﻿Public Class Form1
-    Dim alpha As Int16 ' <---  number between 0 (totally transparent) and 255 (totally opaque)
-    Dim panelName As String
-
+﻿Public Class Main
     Private Sub InfoBtn_Click(sender As Object, e As EventArgs) Handles InfoBtn.Click
-        If MainPnl.Visible = True Then
-            panelName = MainPnl.Name
-
-        ElseIf BotPnl.Visible = True Then
-            panelName = BotPnl.Name
-
-        ElseIf QueuePnl.Visible = True Then
-            panelName = QueuePnl.Name
-        End If
         InfoPnl.Visible = True
         MainPnl.Visible = False
     End Sub
 
     Private Sub InfoBackBtn_Click(sender As Object, e As EventArgs) Handles InfoBackBtn.Click
-        If panelName = MainPnl.Name Then
-            InfoPnl.Visible = False
-            MainPnl.Visible = True
-        ElseIf panelName = BotPnl.Name Then
-            InfoPnl.Visible = False
-            BotPnl.Visible = True
-        ElseIf panelName = QueuePnl.Name Then
-            InfoPnl.Visible = False
-            QueuePnl.Visible = True
-        End If
+        InfoPnl.Visible = False
+        MainPnl.Visible = True
     End Sub
 
     Private Sub BotBtn_Click(sender As Object, e As EventArgs) Handles BotBtn.Click
@@ -66,19 +46,42 @@
         BotPnl.Visible = True
         QueuePnl.Visible = False
     End Sub
-
-    Private Sub CameraImg_GiveFeedback(sender As Object, e As GiveFeedbackEventArgs) Handles CameraImg.GiveFeedback
-
+    Private Sub UpBtn_Click(sender As Object, e As EventArgs) Handles UpBtn.Click
+        Dim NewLocation As New Object
+        If (CameraImg.Location.Y > -100) Then
+            NewLocation = New System.Drawing.Point(CameraImg.Location.X, 0)
+        Else
+            NewLocation = New System.Drawing.Point(CameraImg.Location.X, CameraImg.Location.Y + 100)
+        End If
+        CameraImg.Location = NewLocation
     End Sub
 
-    'Private Sub InfoLbl_Click(sender As Object, e As EventArgs) Handles InfoBtn.Click
-    '    panelName = 
-    'End Sub
+    Private Sub DownBtn_Click(sender As Object, e As EventArgs) Handles DownBtn.Click
+        Dim NewLocation As New Object
+        If (CameraImg.Location.Y < CameraImg.Image.Height * -1 + CameraPnl.Size.Height + 100) Then
+            NewLocation = New System.Drawing.Point(CameraImg.Location.X, CameraImg.Image.Height * -1 + CameraPnl.Size.Height)
+        Else
+            NewLocation = New System.Drawing.Point(CameraImg.Location.X, CameraImg.Location.Y - 100)
+        End If
+        CameraImg.Location = NewLocation
+    End Sub
+    Private Sub LeftBtn_Click(sender As Object, e As EventArgs) Handles LeftBtn.Click
+        Dim NewLocation As New Object
+        If (CameraImg.Location.X > -100) Then
+            NewLocation = New System.Drawing.Point(0, CameraImg.Location.Y)
+        Else
+            NewLocation = New System.Drawing.Point(CameraImg.Location.X + 100, CameraImg.Location.Y)
+        End If
+        CameraImg.Location = NewLocation
+    End Sub
+    Private Sub RightBtn_Click(sender As Object, e As EventArgs) Handles RightBtn.Click
+        Dim NewLocation As New Object
+        If (CameraImg.Location.X < CameraImg.Image.Width * -1 + CameraPnl.Size.Width + 100) Then
+            NewLocation = New System.Drawing.Point(CameraImg.Image.Width * -1 + CameraPnl.Size.Width, CameraImg.Location.Y)
+        Else
+            NewLocation = New System.Drawing.Point(CameraImg.Location.X - 100, CameraImg.Location.Y)
+        End If
+        CameraImg.Location = NewLocation
+    End Sub
 
-
-
-    'Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick, InfoBtn.VisibleChanged
-    '    alpha = 60
-    '    InfoBtn.BackColor = Color.Black
-    'End Sub
 End Class
