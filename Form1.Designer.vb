7dc9364ec8bae5d7f51c92c1b799d23503353d76
drazhi <drazhi@live.ca>
2017-11-16 15:06:34 -0500

    Add files via upload
    InfoBackBtn implemented for any page
    Owl added to look cool

---
+++ Form1.Designer.vb
@@ -0,0 +1,319 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MainPnl = New System.Windows.Forms.Panel()
+        Me.InfoBtn = New System.Windows.Forms.Button()
+        Me.BotBtn = New System.Windows.Forms.Button()
+        Me.InfoPnl = New System.Windows.Forms.Panel()
+        Me.InfoBackBtn = New System.Windows.Forms.Button()
+        Me.InfoLbl = New System.Windows.Forms.Label()
+        Me.BotPnl = New System.Windows.Forms.Panel()
+        Me.PenguinBtn = New System.Windows.Forms.Button()
+        Me.TigerBtn = New System.Windows.Forms.Button()
+        Me.BotBackBtn = New System.Windows.Forms.Button()
+        Me.QueuePnl = New System.Windows.Forms.Panel()
+        Me.QueueBackBtn = New System.Windows.Forms.Button()
+        Me.SpectateBtn = New System.Windows.Forms.Button()
+        Me.QueueBtn = New System.Windows.Forms.Button()
+        Me.QueueGrp = New System.Windows.Forms.GroupBox()
+        Me.QCancelBtn = New System.Windows.Forms.Button()
+        Me.QAcceptBtn = New System.Windows.Forms.Button()
+        Me.QueueLbl = New System.Windows.Forms.Label()
+        Me.CameraPnl = New System.Windows.Forms.Panel()
+        Me.CameraImg = New System.Windows.Forms.PictureBox()
+        Me.MainPnl.SuspendLayout()
+        Me.InfoPnl.SuspendLayout()
+        Me.BotPnl.SuspendLayout()
+        Me.QueuePnl.SuspendLayout()
+        Me.QueueGrp.SuspendLayout()
+        Me.CameraPnl.SuspendLayout()
+        CType(Me.CameraImg, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MainPnl
+        '
+        Me.MainPnl.Controls.Add(Me.BotBtn)
+        Me.MainPnl.Location = New System.Drawing.Point(6, 6)
+        Me.MainPnl.Margin = New System.Windows.Forms.Padding(2)
+        Me.MainPnl.Name = "MainPnl"
+        Me.MainPnl.Size = New System.Drawing.Size(615, 483)
+        Me.MainPnl.TabIndex = 0
+        '
+        'InfoBtn
+        '
+        Me.InfoBtn.BackColor = System.Drawing.Color.Transparent
+        Me.InfoBtn.BackgroundImage = Global.TourBot.My.Resources.Resources._1828608
+        Me.InfoBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.InfoBtn.FlatAppearance.BorderSize = 0
+        Me.InfoBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.InfoBtn.ForeColor = System.Drawing.Color.Black
+        Me.InfoBtn.Location = New System.Drawing.Point(486, 321)
+        Me.InfoBtn.Margin = New System.Windows.Forms.Padding(0)
+        Me.InfoBtn.Name = "InfoBtn"
+        Me.InfoBtn.Size = New System.Drawing.Size(130, 162)
+        Me.InfoBtn.TabIndex = 0
+        Me.InfoBtn.Text = "Need help?"
+        Me.InfoBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.InfoBtn.UseVisualStyleBackColor = False
+        '
+        'BotBtn
+        '
+        Me.BotBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BotBtn.Location = New System.Drawing.Point(135, 174)
+        Me.BotBtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.BotBtn.Name = "BotBtn"
+        Me.BotBtn.Size = New System.Drawing.Size(310, 176)
+        Me.BotBtn.TabIndex = 1
+        Me.BotBtn.Text = "See Bots"
+        Me.BotBtn.UseVisualStyleBackColor = True
+        '
+        'InfoPnl
+        '
+        Me.InfoPnl.Controls.Add(Me.InfoBackBtn)
+        Me.InfoPnl.Controls.Add(Me.InfoLbl)
+        Me.InfoPnl.Location = New System.Drawing.Point(6, 6)
+        Me.InfoPnl.Margin = New System.Windows.Forms.Padding(2)
+        Me.InfoPnl.Name = "InfoPnl"
+        Me.InfoPnl.Size = New System.Drawing.Size(615, 483)
+        Me.InfoPnl.TabIndex = 0
+        Me.InfoPnl.Visible = False
+        '
+        'InfoBackBtn
+        '
+        Me.InfoBackBtn.Location = New System.Drawing.Point(2, 2)
+        Me.InfoBackBtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.InfoBackBtn.Name = "InfoBackBtn"
+        Me.InfoBackBtn.Size = New System.Drawing.Size(84, 59)
+        Me.InfoBackBtn.TabIndex = 1
+        Me.InfoBackBtn.Text = "InfoBackBtn"
+        Me.InfoBackBtn.UseVisualStyleBackColor = True
+        '
+        'InfoLbl
+        '
+        Me.InfoLbl.AutoSize = True
+        Me.InfoLbl.Location = New System.Drawing.Point(13, 105)
+        Me.InfoLbl.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.InfoLbl.Name = "InfoLbl"
+        Me.InfoLbl.Size = New System.Drawing.Size(0, 13)
+        Me.InfoLbl.TabIndex = 0
+        '
+        'BotPnl
+        '
+        Me.BotPnl.Controls.Add(Me.PenguinBtn)
+        Me.BotPnl.Controls.Add(Me.TigerBtn)
+        Me.BotPnl.Controls.Add(Me.BotBackBtn)
+        Me.BotPnl.Location = New System.Drawing.Point(6, 6)
+        Me.BotPnl.Margin = New System.Windows.Forms.Padding(2)
+        Me.BotPnl.Name = "BotPnl"
+        Me.BotPnl.Size = New System.Drawing.Size(615, 483)
+        Me.BotPnl.TabIndex = 1
+        Me.BotPnl.Visible = False
+        '
+        'PenguinBtn
+        '
+        Me.PenguinBtn.Location = New System.Drawing.Point(172, 105)
+        Me.PenguinBtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.PenguinBtn.Name = "PenguinBtn"
+        Me.PenguinBtn.Size = New System.Drawing.Size(94, 75)
+        Me.PenguinBtn.TabIndex = 2
+        Me.PenguinBtn.Text = "Penguin"
+        Me.PenguinBtn.UseVisualStyleBackColor = True
+        '
+        'TigerBtn
+        '
+        Me.TigerBtn.Location = New System.Drawing.Point(56, 107)
+        Me.TigerBtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.TigerBtn.Name = "TigerBtn"
+        Me.TigerBtn.Size = New System.Drawing.Size(89, 63)
+        Me.TigerBtn.TabIndex = 1
+        Me.TigerBtn.Text = "Tiger"
+        Me.TigerBtn.UseVisualStyleBackColor = True
+        '
+        'BotBackBtn
+        '
+        Me.BotBackBtn.Location = New System.Drawing.Point(2, 2)
+        Me.BotBackBtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.BotBackBtn.Name = "BotBackBtn"
+        Me.BotBackBtn.Size = New System.Drawing.Size(88, 53)
+        Me.BotBackBtn.TabIndex = 0
+        Me.BotBackBtn.Text = "Back"
+        Me.BotBackBtn.UseVisualStyleBackColor = True
+        '
+        'QueuePnl
+        '
+        Me.QueuePnl.Controls.Add(Me.QueueBackBtn)
+        Me.QueuePnl.Controls.Add(Me.SpectateBtn)
+        Me.QueuePnl.Controls.Add(Me.QueueBtn)
+        Me.QueuePnl.Location = New System.Drawing.Point(6, 6)
+        Me.QueuePnl.Margin = New System.Windows.Forms.Padding(2)
+        Me.QueuePnl.Name = "QueuePnl"
+        Me.QueuePnl.Size = New System.Drawing.Size(615, 483)
+        Me.QueuePnl.TabIndex = 2
+        Me.QueuePnl.Visible = False
+        '
+        'QueueBackBtn
+        '
+        Me.QueueBackBtn.Location = New System.Drawing.Point(2, 2)
+        Me.QueueBackBtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.QueueBackBtn.Name = "QueueBackBtn"
+        Me.QueueBackBtn.Size = New System.Drawing.Size(84, 59)
+        Me.QueueBackBtn.TabIndex = 2
+        Me.QueueBackBtn.Text = "Back"
+        Me.QueueBackBtn.UseVisualStyleBackColor = True
+        '
+        'SpectateBtn
+        '
+        Me.SpectateBtn.Location = New System.Drawing.Point(334, 209)
+        Me.SpectateBtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.SpectateBtn.Name = "SpectateBtn"
+        Me.SpectateBtn.Size = New System.Drawing.Size(135, 105)
+        Me.SpectateBtn.TabIndex = 1
+        Me.SpectateBtn.Text = "Spectate"
+        Me.SpectateBtn.UseVisualStyleBackColor = True
+        '
+        'QueueBtn
+        '
+        Me.QueueBtn.Location = New System.Drawing.Point(94, 198)
+        Me.QueueBtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.QueueBtn.Name = "QueueBtn"
+        Me.QueueBtn.Size = New System.Drawing.Size(130, 99)
+        Me.QueueBtn.TabIndex = 0
+        Me.QueueBtn.Text = "Queue"
+        Me.QueueBtn.UseVisualStyleBackColor = True
+        '
+        'QueueGrp
+        '
+        Me.QueueGrp.Controls.Add(Me.QCancelBtn)
+        Me.QueueGrp.Controls.Add(Me.QAcceptBtn)
+        Me.QueueGrp.Controls.Add(Me.QueueLbl)
+        Me.QueueGrp.Location = New System.Drawing.Point(476, 2)
+        Me.QueueGrp.Margin = New System.Windows.Forms.Padding(2)
+        Me.QueueGrp.Name = "QueueGrp"
+        Me.QueueGrp.Padding = New System.Windows.Forms.Padding(2)
+        Me.QueueGrp.Size = New System.Drawing.Size(148, 168)
+        Me.QueueGrp.TabIndex = 3
+        Me.QueueGrp.TabStop = False
+        Me.QueueGrp.Visible = False
+        '
+        'QCancelBtn
+        '
+        Me.QCancelBtn.Location = New System.Drawing.Point(82, 135)
+        Me.QCancelBtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.QCancelBtn.Name = "QCancelBtn"
+        Me.QCancelBtn.Size = New System.Drawing.Size(54, 21)
+        Me.QCancelBtn.TabIndex = 2
+        Me.QCancelBtn.Text = "Cancel"
+        Me.QCancelBtn.UseVisualStyleBackColor = True
+        '
+        'QAcceptBtn
+        '
+        Me.QAcceptBtn.Location = New System.Drawing.Point(8, 135)
+        Me.QAcceptBtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.QAcceptBtn.Name = "QAcceptBtn"
+        Me.QAcceptBtn.Size = New System.Drawing.Size(62, 24)
+        Me.QAcceptBtn.TabIndex = 1
+        Me.QAcceptBtn.Text = "Accept"
+        Me.QAcceptBtn.UseVisualStyleBackColor = True
+        '
+        'QueueLbl
+        '
+        Me.QueueLbl.AutoSize = True
+        Me.QueueLbl.Location = New System.Drawing.Point(15, 24)
+        Me.QueueLbl.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.QueueLbl.Name = "QueueLbl"
+        Me.QueueLbl.Size = New System.Drawing.Size(48, 13)
+        Me.QueueLbl.TabIndex = 0
+        Me.QueueLbl.Text = "in queue"
+        '
+        'CameraPnl
+        '
+        Me.CameraPnl.Controls.Add(Me.CameraImg)
+        Me.CameraPnl.Location = New System.Drawing.Point(6, 6)
+        Me.CameraPnl.Margin = New System.Windows.Forms.Padding(2)
+        Me.CameraPnl.Name = "CameraPnl"
+        Me.CameraPnl.Size = New System.Drawing.Size(616, 483)
+        Me.CameraPnl.TabIndex = 4
+        Me.CameraPnl.Visible = False
+        '
+        'CameraImg
+        '
+        Me.CameraImg.ImageLocation = ""
+        Me.CameraImg.Location = New System.Drawing.Point(2, 2)
+        Me.CameraImg.Margin = New System.Windows.Forms.Padding(2)
+        Me.CameraImg.Name = "CameraImg"
+        Me.CameraImg.Size = New System.Drawing.Size(614, 480)
+        Me.CameraImg.TabIndex = 0
+        Me.CameraImg.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(632, 513)
+        Me.Controls.Add(Me.InfoBtn)
+        Me.Controls.Add(Me.QueueGrp)
+        Me.Controls.Add(Me.MainPnl)
+        Me.Controls.Add(Me.CameraPnl)
+        Me.Controls.Add(Me.InfoPnl)
+        Me.Controls.Add(Me.BotPnl)
+        Me.Controls.Add(Me.QueuePnl)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.MaximumSize = New System.Drawing.Size(648, 551)
+        Me.MinimumSize = New System.Drawing.Size(648, 551)
+        Me.Name = "Form1"
+        Me.Text = "TourBot"
+        Me.MainPnl.ResumeLayout(False)
+        Me.InfoPnl.ResumeLayout(False)
+        Me.InfoPnl.PerformLayout()
+        Me.BotPnl.ResumeLayout(False)
+        Me.QueuePnl.ResumeLayout(False)
+        Me.QueueGrp.ResumeLayout(False)
+        Me.QueueGrp.PerformLayout()
+        Me.CameraPnl.ResumeLayout(False)
+        CType(Me.CameraImg, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MainPnl As Panel
+    Friend WithEvents InfoBtn As Button
+    Friend WithEvents InfoPnl As Panel
+    Friend WithEvents InfoLbl As Label
+    Friend WithEvents InfoBackBtn As Button
+    Friend WithEvents BotPnl As Panel
+    Friend WithEvents BotBtn As Button
+    Friend WithEvents QueuePnl As Panel
+    Friend WithEvents SpectateBtn As Button
+    Friend WithEvents QueueBtn As Button
+    Friend WithEvents QueueGrp As GroupBox
+    Friend WithEvents QCancelBtn As Button
+    Friend WithEvents QAcceptBtn As Button
+    Friend WithEvents QueueLbl As Label
+    Friend WithEvents BotBackBtn As Button
+    Friend WithEvents PenguinBtn As Button
+    Friend WithEvents TigerBtn As Button
+    Friend WithEvents QueueBackBtn As Button
+    Friend WithEvents CameraPnl As Panel
+    Friend WithEvents CameraImg As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+    Dim alpha As Int16 ' <---  number between 0 (totally transparent) and 255 (totally opaque)
+    Dim panelName As String
+
+    Private Sub InfoBtn_Click(sender As Object, e As EventArgs) Handles InfoBtn.Click
+        If MainPnl.Visible = True Then
+            panelName = MainPnl.Name
+
+        ElseIf BotPnl.Visible = True Then
+            panelName = BotPnl.Name
+
+        ElseIf QueuePnl.Visible = True Then
+            panelName = QueuePnl.Name
+        End If
+        InfoPnl.Visible = True
+        MainPnl.Visible = False
+    End Sub
+
+    Private Sub InfoBackBtn_Click(sender As Object, e As EventArgs) Handles InfoBackBtn.Click
+        If panelName = MainPnl.Name Then
+            InfoPnl.Visible = False
+            MainPnl.Visible = True
+        ElseIf panelName = BotPnl.Name Then
+            InfoPnl.Visible = False
+            BotPnl.Visible = True
+        ElseIf panelName = QueuePnl.Name Then
+            InfoPnl.Visible = False
+            QueuePnl.Visible = True
+        End If
+    End Sub
+
+    Private Sub BotBtn_Click(sender As Object, e As EventArgs) Handles BotBtn.Click
+        BotPnl.Visible = True
+        MainPnl.Visible = False
+    End Sub
+
+    Private Sub QueueBtn_Click(sender As Object, e As EventArgs) Handles QueueBtn.Click
+        QueueGrp.Visible = True
+    End Sub
+
+    Private Sub QAcceptBtn_Click(sender As Object, e As EventArgs) Handles QAcceptBtn.Click
+        QueueGrp.Visible = False
+        CameraPnl.Visible = True
+    End Sub
+
+    Private Sub QCancelBtn_Click(sender As Object, e As EventArgs) Handles QCancelBtn.Click
+        QueueGrp.Visible = False
+    End Sub
+
+    Private Sub BotBackBtn_Click(sender As Object, e As EventArgs) Handles BotBackBtn.Click
+        BotPnl.Visible = False
+        MainPnl.Visible = True
+    End Sub
+
+    Private Sub TigerBtn_Click(sender As Object, e As EventArgs) Handles TigerBtn.Click
+        BotPnl.Visible = False
+        QueuePnl.Visible = True
+    End Sub
+
+    Private Sub PenguinBtn_Click(sender As Object, e As EventArgs) Handles PenguinBtn.Click
+        BotPnl.Visible = False
+        QueuePnl.Visible = True
+    End Sub
+
+    Private Sub QueueBackBtn_Click(sender As Object, e As EventArgs) Handles QueueBackBtn.Click
+        BotPnl.Visible = True
+        QueuePnl.Visible = False
+    End Sub
+
+    Private Sub CameraImg_GiveFeedback(sender As Object, e As GiveFeedbackEventArgs) Handles CameraImg.GiveFeedback
+
+    End Sub
+
+    'Private Sub InfoLbl_Click(sender As Object, e As EventArgs) Handles InfoBtn.Click
+    '    panelName = 
+    'End Sub
+
+
+
+    'Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick, InfoBtn.VisibleChanged
+    '    alpha = 60
+    '    InfoBtn.BackColor = Color.Black
+    'End Sub
+End Class
